f1dd5ff0b3c1fea27fd407b715e59d4b5d9d5185
JLeitzinger <elaboratejohnny@gmail.com>
2024-05-18 10:41:56 -0400

    another fix

--- main_game/role.upgrader.js
+++ main_game/role.upgrader.js
@@ -25,7 +25,7 @@ var roleUpgrader = {
                 creep.moveTo(controller_x - 1, controller_y);
             }
         }
-        if(creep.store.getFreeCapacity() > 0) {
+        if(creep.store.getFreeCapacity() > 0 && !creep.memory.upgrading) {
             var sources = creep.room.find(FIND_SOURCES);
             if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
