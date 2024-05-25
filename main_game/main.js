9a775d98647c6c8a222d7e0086384bd75d6d218d
JLeitzinger <elaboratejohnny@gmail.com>
2024-05-25 12:39:51 -0400

    updating number of creeps

--- main_game/main.js
+++ main_game/main.js
@@ -13,9 +13,9 @@ var spotsInLine = new Map();
 var resourceAssignments = new Map();
 
 // Define # of Creeps total
-totalCreeps.set('harvester', 4);
-totalCreeps.set('upgrader', 2);
-totalCreeps.set('builder', 1);
+totalCreeps.set('harvester', 1);
+totalCreeps.set('upgrader', 1);
+totalCreeps.set('builder', 0);
 
 
 module.exports.loop = function () {
