a4f8edfc7984c8af5991eff0a24f7af95b04dbf4
JLeitzinger <elaboratejohnny@gmail.com>
2024-05-26 18:54:16 -0400

    added function to upgrader

--- main_game/Testing/role.harvester.js
+++ main_game/Testing/role.harvester.js
@@ -10,22 +10,6 @@ var roleHarvester = {
         else {
 
             funcCreeps.findStores(creep);
-            // console.log(creep.name + ' going back to spawn');
-            // var targets = creep.room.find(FIND_STRUCTURES, {
-            //         filter: (structure) => {
-            //             return (structure.structureType == STRUCTURE_EXTENSION || 
-            //                     structure.structureType == STRUCTURE_SPAWN ||
-            //                     structure.structureType == STRUCTURE_TOWER) &&
-            //                     structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
-            //         }
-            // });
-            // if(targets.length > 0) {
-            //     console.log(targets[0])
-            //     console.log(creep.name)
-            //     if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-            //         creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
-            //     }
-            // }
         }
 	}
 };
