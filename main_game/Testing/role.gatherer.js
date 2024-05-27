740b71eae656c4159d4876642029577626a8d216
JLeitzinger <elaboratejohnny@gmail.com>
2024-05-27 10:01:16 -0400

    fixed gatherer

--- main_game/Testing/role.gatherer.js
+++ main_game/Testing/role.gatherer.js
@@ -2,7 +2,7 @@ var funcCreeps = require("func.creeps");
 
 var roleGatherer = {
     run: function(creep) {
-        if(creep.store.getFreeCapacity() == 0) {
+        if(creep.store.getFreeCapacity() != 0) {
             let drops = creep.room.find(FIND_DROPPED_RESOURCES);
             if (drops.length > 1) {
                 if(creep.pickup(drops[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
