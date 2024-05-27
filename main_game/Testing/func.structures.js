ae52ab6fd0a6bd06edb4572d1f912465e48dc365
JLeitzinger <elaboratejohnny@gmail.com>
2024-05-27 09:50:25 -0400

    checking total function

--- main_game/Testing/func.structures.js
+++ main_game/Testing/func.structures.js
@@ -2,6 +2,7 @@
 var funcStructures = {
     countCreeps: function(role) {
         const totalCreeps = _.filter(Game.creeps, (creep) => creep.memory.role == role);
+        console.log('total ' + role + ': ' + totalCreeps);
         return totalCreeps.length
     }
 }
