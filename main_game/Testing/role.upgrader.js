9db0bef02f7e983cee39eb87b2c23c1db16d0117
JLeitzinger <elaboratejohnny@gmail.com>
2024-05-26 23:22:17 -0400

    I'm an idiot

--- main_game/Testing/role.upgrader.js
+++ main_game/Testing/role.upgrader.js
@@ -10,8 +10,7 @@ var roleUpgrader = {
             creep.say('🔄 harvest');
 	    }
 	    if(!creep.memory.upgrading &&
-            creep.store.getFreeCapacity() == 0 &&
-            Game.spawns['spawn1'].store.getFreeCapacity(RESOURCE_ENERGY)<100) {
+            creep.store.getFreeCapacity() == 0) {
 	        creep.memory.upgrading = true;
 	        creep.say('⚡ upgrade');
 	    }
@@ -20,33 +19,16 @@ var roleUpgrader = {
             if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(creep.room.controller, {visualizePathStyle: {stroke: '#ffffff'}});
             }
-            else {
-                var controller_x = creep.room.controller.pos.x;
-                var controller_y = creep.room.controller.pos.y;
-
-                creep.moveTo(controller_x - 1, controller_y);
-            }
         }
-        if(creep.store.getFreeCapacity() > 0 && !creep.memory.upgrading) {
-            var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
-            }
+        else{
+            console.log('upgrader should be harvesting')
+            funcCreeps.harvest(creep);
         }
 
-        else {
-            var targets = creep.room.find(FIND_STRUCTURES, {
-                filter: (structure) => {
-                    return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
-                        structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
-                }
-            });
-            if(targets.length > 0) {
-                if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
-                }
-            }
-        }
+        // else {
+        //     console.log('upgrader capacity: ' + creep.store.getFreeCapacity());
+        //     funcCreeps.depositStores(creep);
+        // }
 	}
 };
 
