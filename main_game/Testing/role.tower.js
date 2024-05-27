e83cd94b9652c8bae873276023df9d67548bd808
JLeitzinger <elaboratejohnny@gmail.com>
2024-05-27 13:08:14 -0400

    added a check

--- main_game/Testing/role.tower.js
+++ main_game/Testing/role.tower.js
@@ -2,6 +2,7 @@ var funcStructures = require('func.structures');
 
 var roleTower = {
     run: function(tower) {
+        console.log("This is tower in fn: " + tower);
         const enemies = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
         
         if (enemies.length > 0) {
