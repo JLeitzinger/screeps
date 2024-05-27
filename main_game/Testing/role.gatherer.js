3ee4d3794c51f462167af5972fbaf495fca8b42c
JLeitzinger <elaboratejohnny@gmail.com>
2024-05-27 10:29:36 -0400

    trying this again

--- main_game/Testing/role.gatherer.js
+++ main_game/Testing/role.gatherer.js
@@ -7,11 +7,6 @@ var roleGatherer = {
             let harvesters = creep.room.find(FIND_MY_CREEPS, {
                 filter: (creep) => creep.memory.role == 'harvester'
             });
-            console.log(harvesters);
-            if (harvesters > 0) {
-                creep.moveTo(harvesters[0])
-                console.log(creep + ' moving to ' + harvesters[0]);
-            }
             if (drops.length > 0) {
                 if(creep.pickup(drops[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(drops[0], {visualizePathStyle: {stroke: '#ffffff'}});
