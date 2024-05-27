bbb0b23f595e8c12e2bf95556a2ded26ccad05f6
JLeitzinger <elaboratejohnny@gmail.com>
2024-05-27 09:09:32 -0400

    checking harvestor

--- main_game/Testing/role.harvester.js
+++ main_game/Testing/role.harvester.js
@@ -5,10 +5,11 @@ var roleHarvester = {
     /** @param {Creep} creep **/
     run: function(creep) {
 	    if(creep.store.getFreeCapacity() > 0) {
+            creep.memory.harvesting = true;
             funcCreeps.harvest(creep);
             }
         else {
-
+            creep.memory.harvesting = false;
             funcCreeps.depositStores(creep);
         }
 	}
