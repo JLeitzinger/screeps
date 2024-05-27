ef19f0a60edf244369d1e1d141ee1536bc66a5b1
JLeitzinger <elaboratejohnny@gmail.com>
2024-05-27 11:03:43 -0400

    updating console log in gatherer

--- main_game/Testing/role.gatherer.js
+++ main_game/Testing/role.gatherer.js
@@ -4,13 +4,10 @@ var roleGatherer = {
     run: function(creep) {
         if(creep.store.getFreeCapacity() != 0) {
             let drops = creep.room.find(FIND_DROPPED_RESOURCES);
-            let harvesters = creep.room.find(FIND_MY_CREEPS, {
-                filter: (creep) => creep.memory.role == 'harvester'
-            });
             if (drops.length > 0) {
                 if(creep.pickup(drops[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(drops[0], {visualizePathStyle: {stroke: '#ffffff'}});
-                    console.log(creep + ' moving to ' + drops[0])
+                    // console.log(creep + ' moving to ' + drops[0])
                 }
             }
         }
