fac3a2d7e508e06c7604213d72b00924f99b13e9
JLeitzinger <elaboratejohnny@gmail.com>
2024-05-27 13:18:34 -0400

    testing

--- main_game/Testing/role.tower.js
+++ main_game/Testing/role.tower.js
@@ -3,10 +3,10 @@ var funcStructures = require('func.structures');
 var roleTower = {
     run: function(tower) {
         console.log("This is tower in fn: " + tower);
-        const enemies = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+        const enemy = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
         
-        if (enemies.length>0) {
-            tower.attack(enemies[0]);
+        if (enemy != undefined) {
+            tower.attack(enemy);
         }
     }
 }; 
