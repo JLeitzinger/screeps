314ccafab7b4cf7f78ea1a06d80215c6f8ab18ee
JLeitzinger <elaboratejohnny@gmail.com>
2024-05-26 22:30:22 -0400

    test

--- main_game/Testing/func.creeps.js
+++ main_game/Testing/func.creeps.js
@@ -4,7 +4,7 @@ funcSources = require("func.sources");
 var funcCreeps = {
     harvest: function(creep) {
         var source = Game.getObjectById(creep.memory.resource);
-        console.log(creep + ' harvesting ' + source);
+        // console.log(creep + ' harvesting ' + source);
         if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
             creep.moveTo(source, {visualizePathStyle: {stroke: '#ffaa00'}});
         }
